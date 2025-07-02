4ef4e858da87f90230f8329b529759a96fd43035
Mark Szulc <mszulc@adobe.com>
2025-07-02 13:58:33 +1000

    feat: add blog post list component with model and styles

---
+++ blocks/bloglist/bloglist.js
@@ -0,0 +1,34 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  const indexResponse = await fetch('/sitemap.json');
+  if (!indexResponse.ok) {
+    return;
+  }
+
+  const index = await indexResponse.json();
+
+  const container = document.createElement('ul');
+
+  index.data
+    .forEach((post) => {
+      if (post.category !== 'blog') {
+        return;
+      }
+      const eager = false;
+      const title = '';
+      const li = document.createElement('li');
+      const picture = createOptimizedPicture(post.image, post.title || title, eager, [{ width: '750' }]);
+      const pictureTag = picture.outerHTML;
+
+      li.innerHTML = `
+      <a href="${post.path}">
+        ${pictureTag}
+        <h4>${post.title}</h4>
+      </a>
+    `;
+      container.append(li);
+    });
+
+  block.append(container);
+}
