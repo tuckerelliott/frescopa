5896a1aeb3db79c7da0c6a3704abeb628acdd3c0
Mark Szulc <mszulc@adobe.com>
2025-07-02 13:49:06 +1000

    feat: update store locator to use dynamic text for search title and placeholder

--- blocks/store-locator/store-locator.js
+++ blocks/store-locator/store-locator.js
@@ -27,6 +27,9 @@ export async function initMap() {
 
 export default function decorate(block) {
   const pText = block.querySelector('p').textContent;
+  const subhead = block.querySelectorAll('p')[1].textContent;
+  const searchplaceholder = block.querySelectorAll('p')[2].textContent;
+  const buttontext = block.querySelectorAll('p')[3].textContent;
   block.textContent = '';  
   window.initMap = async () => {
     initMap();
@@ -37,10 +40,10 @@ export default function decorate(block) {
     <div class="sidepanel">
       <h3 class="sidepanel__title">${pText}</h3>
     <div class="search">
-      <p class="search__title">Find another location</p>
+      <p class="search__title">${subhead}</p>
       <div class="search__box">
-        <input class="search__input" type="text" placeholder="Zip Code" name="search"></input>
-        <button class="search__button">Search</button>
+        <input class="search__input" type="text" placeholder="${searchplaceholder}" name="search"></input>
+        <button class="search__button">${buttontext}</button>
       </div>
     </div>
     </div>
