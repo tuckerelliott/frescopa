10fc0031f411a98636c284d5a6e762309eb7ed26
Mark Szulc <mszulc@adobe.com>
2025-07-11 19:47:57 +1000

    fix: remove unnecessary blank line in offer block decoration

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -12,7 +12,6 @@ export default async function decorate(block) {
     variationname = variationElem.innerHTML.trim();
   }
 
-
   const url = window.location && window.location.origin && window.location.origin.includes('author')
     ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
     : `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
