b917d39ae506ecb19ce6d4f01c2c5931ee3c0d20
Mark Szulc <mszulc@adobe.com>
2025-07-11 16:41:36 +1000

    fix: ensure default variation name is set correctly in Offer block

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -6,10 +6,12 @@ export default async function decorate(block) {
   const aemauthorurl = getAEMAuthor();
   const persistedquery = '/graphql/execute.json/frescopa/OfferByPath';
   const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
-  let variationname = block.querySelector(':scope div:nth-child(2) > div > p').innerHTML.trim();
-  if (!variationname) {
-    variationname = 'main';
+  let variationname = 'main';
+  const variationElem = block.querySelector(':scope div:nth-child(2) > div > p');
+  if (variationElem && variationElem.innerHTML) {
+    variationname = variationElem.innerHTML.trim();
   }
+
 
   const url = window.location && window.location.origin && window.location.origin.includes('author')
     ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
